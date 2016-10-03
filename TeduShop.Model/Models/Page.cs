f04dda41809c5f79d88ec1201cf944f6759fb63a
xuantung95tt <xuantung95tt@gmail.com>
2016-10-03 14:04:01 +0700

    Update Models

--- TeduShop.Model/Models/Page.cs
+++ TeduShop.Model/Models/Page.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TeduShop.Model.Models
 {
     [Table("Pages")]
-    class Page
+    internal class Page
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         public string Content { get; set; }
         public bool Status { get; set; }
+
         [MaxLength(500)]
         public string MetaKeyword { get; set; }
+
         [MaxLength(500)]
         public string MetaDescription { get; set; }
     }
